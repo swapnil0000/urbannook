11ba3b7f6f32d615fe155227e8539f6983425475
swapnil0000 <swapnilsrivastava1999@gmail.com>
2025-12-07 19:23:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./component/layout/Header"
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Header/>
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold text-blue-600">Welcome to Urban Nook</h1>
+        <p className="text-gray-700 mt-2">Tailwind CSS is working!</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '@/component/library/Button'
+const Product = () => {
+  return (
+    <div>
+      <Button/>
+    </div>
+  )
+}
+
+export default Product
