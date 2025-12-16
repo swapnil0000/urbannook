14933a148900a01d1ef6a78142f1fa40bc7bd334
Swapnil Srivastav <swapnilsrivastava1999@gmail.com>
2025-12-16 14:00:40 +0530

    checking account visibilty

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,7 +3,7 @@ import UserProfile from '../feature/auth/components/UserProfile';
 
 export default function ProfilePage() {
   return (
-    <div className="min-h-screen bg-bgSecondary py-12 px-4">
+    <div className="min-h-screen bg-bgSecondary py-12 px-5">
       <UserProfile />
     </div>
   );
