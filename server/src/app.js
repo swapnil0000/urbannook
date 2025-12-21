359b2f5c72c44b47bf5584416d4ad5f215c3a9eb
Yash Kothari <yashk8119@gmail.com>
2025-12-21 22:53:40 +0530

    cors option updated and controllers updated

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,25 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
-import { userRouter ,adminRouter} from "./routes/index.js";
+import { userRouter, adminRouter } from "./routes/index.js";
 import cookieParser from "cookie-parser";
 dotenv.config({
   path: "./.env",
 });
 const app = express();
+const whiteListClientUrl = process.env.WHITE_LIST_CLIENT_URI.split(",");
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || whiteListClientUrl.includes(origin.trim())) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
