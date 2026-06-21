502b882a2d9307be485a59223d88cce213e02db4
ADDA <adda@ip-192-168-1-37.ap-south-1.compute.internal>
2026-06-21 09:08:11 +0530

    add hours text

--- client/src/pages/shop/ProductDetailPage.jsx
+++ client/src/pages/shop/ProductDetailPage.jsx
@@ -1010,7 +1010,7 @@ const ProductDetailPage = () => {
                   <i className="fa-solid fa-truck text-[#F5DEB3] text-sm"></i>
                   <span className="text-[9px] text-[#F5DEB3]/70 uppercase tracking-wider font-bold leading-tight">
                     {" "}
-                  24–48 Estimated 
+                  24–48 Hours Estimated 
                     <br />
                     Shipment
                   </span>
