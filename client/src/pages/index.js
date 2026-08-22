36ddf9ab93f420fd49cfb5a5fd6716615a2a706f
Joey <yashk8119@gmail.com>
2026-08-22 18:13:56 +0530

    Loyalts Pts

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,4 @@
-import { lazy } from 'react';
+import { lazy } from "react";
 
 // Import HomePage directly for instant loading
 export { default as HomePage } from "./home/HomePage.jsx";
@@ -8,16 +8,24 @@ export const CheckoutPage = lazy(() => import("../pages/CheckoutPage"));
 export const MyOrdersPage = lazy(() => import("./account/MyOrdersPage.jsx"));
 export const WishlistPage = lazy(() => import("./account/WishlistPage.jsx"));
 export const RewardsPage = lazy(() => import("./info/RewardsPage.jsx"));
-export const UnCashCalculationPage = lazy(() => import("./info/UnCashCalculationPage.jsx"));
+export const UnCashCalculationPage = lazy(
+  () => import("./info/UnCashCalculationPage.jsx"),
+);
 export const SettingsPage = lazy(() => import("./account/SettingsPage.jsx"));
 export const TermsConditions = lazy(() => import("./legal/TermsCondition.jsx"));
-export const CancellationPolicy = lazy(() => import("./legal/CancellationPolicy.jsx"));
+export const CancellationPolicy = lazy(
+  () => import("./legal/CancellationPolicy.jsx"),
+);
 export const PrivacyPolicy = lazy(() => import("./legal/PrivacyPolicy.jsx"));
 export const Faq = lazy(() => import("./support/Faqs.jsx"));
 export const Return = lazy(() => import("./legal/Return.jsx"));
 export const ContactPage = lazy(() => import("./support/ContactPage.jsx"));
 export const AllProductsPage = lazy(() => import("./shop/AllProductsPage.jsx"));
-export const ProductDetailPage = lazy(() => import("./shop/ProductDetailPage.jsx"));
+export const ProductDetailPage = lazy(
+  () => import("./shop/ProductDetailPage.jsx"),
+);
 export const MyProfilePage = lazy(() => import("./account/MyProfilePage.jsx"));
-export const CustomerSupportPage = lazy(() => import("./support/CustomerSupportPage.jsx"));
+export const CustomerSupportPage = lazy(
+  () => import("./support/CustomerSupportPage.jsx"),
+);
 export const AboutPage = lazy(() => import("./info/AboutPage.jsx"));
