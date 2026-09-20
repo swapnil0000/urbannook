585f992c4c083344321faa1b8e452fe55d321c53
swapnil0000 <swapnilsrivastav1999@gmail.com>
2026-09-20 17:56:56 +0530

    changes

--- server/src/config/envConfigSetup.js
+++ server/src/config/envConfigSetup.js
@@ -1,4 +1,11 @@
+import dotenv from "dotenv";
+import path from "path";
+import fs from "fs";
+
 const mode = process.env.NODE_ENV || "development";
 
 console.log(`🚀 Mode: ${mode.toUpperCase()}`);
 console.log("🔐 Environment variables are provided by the process environment.");
+
+export default process.env;
+
